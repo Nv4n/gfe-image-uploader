16a555147f87b104435976c056d51c3a49c32f5a
Sybatron <vipsister2014@gmail.com>
2026-04-16 00:29:01 +0300

    update layout and typology

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import ThemeToggle from "@/components/theme-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Flag } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<>
+			<ThemeToggle className="fixed top-4 right-4 border" />
 			<Card className="relative mx-auto flex w-full max-w-xs flex-col gap-6 rounded-lg border border-neutral-200 pt-32 shadow-md before:absolute before:inset-0 before:h-32 before:w-full before:bg-[url(/cover.png)] before:bg-cover before:bg-center before:content-[''] md:max-w-2xl md:pt-44 before:md:h-44 lg:max-w-3xl">
 				<CardHeader className="relative z-1 flex justify-between">
 					<Avatar className="relative -mb-20 size-20 -translate-y-1/2 border border-zinc-200 shadow-sm">
@@ -19,20 +22,37 @@ export default function Home() {
 							/>
 						</AvatarFallback>
 					</Avatar>
-					<Button className="">Update picture</Button>
+					<Button
+						variant="outline"
+						className="mt-2 rounded px-3 py-2 text-sm"
+					>
+						Update picture
+					</Button>
 				</CardHeader>
-				<CardContent>
-					<h1>Jack Smith</h1>
-					<div className="flex gap-3">
-						<span>@kingjack</span>
-						<div className="flex gap-2">
-							<span>Senior Product Designer</span>
-							<span>at Webflow</span>
-							<span className="text-neutral-600">He/Him</span>
+				<CardContent className="flex flex-col gap-6">
+					<h1 className="text-primary text-2xl font-semibold">
+						Jack Smith
+					</h1>
+					<div className="flex flex-col items-start gap-3 text-xl md:flex-row md:items-center">
+						<span className="">@kingjack</span>
+
+						<div className="flex items-center before:hidden md:before:mr-2 md:before:block md:before:content-['•']">
+							<span>Senior Product Designer </span>
 						</div>
 
-						<span className="text-neutral-600">
-							{" "}
+						<div className="flex items-center before:hidden md:before:mr-2 md:before:block md:before:content-['•']">
+							<span className="text-muted-foreground">
+								He/Him
+							</span>
+						</div>
+					</div>
+					<div className="flex gap-4">
+						<Flag
+							role="img"
+							aria-label="Canada Flag"
+							className="text-xl"
+						/>
+						<span className="text-muted-foreground">
 							Vancouver, Canada
 						</span>
 					</div>
